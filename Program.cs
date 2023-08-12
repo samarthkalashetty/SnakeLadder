ebc9a6f89f77a0e3f2264e321e0b369a6abd135c
samarthkalashetty <samarthkalashetti2612@gmail.com>
2023-08-12 12:41:48 +0530

    Welcome to SnakeLadder game

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace SnakeAndLadderGame
+{
+    class UC1
+    {
+        static void Main(string[] args)
+        {
+            int playerPosition = 0;
+            bool gameOver = false;
+            Random random = new Random();
+
+            Console.WriteLine("Welcome to Snake and Ladder Game!");
+
+            while (!gameOver)
+            {
+                Console.WriteLine($"You are currently at position {playerPosition}");
+
+                Console.Write("Press Enter to roll the dice...");
+                Console.ReadLine();
+
+                int diceRoll = random.Next(1, 7); // Simulate dice roll (1 to 6)
+                Console.WriteLine($"You rolled a {diceRoll}");
+
+                playerPosition += diceRoll;
+
+                // Snake and ladder rules...
+                // (same as previous implementation)
+
+                if (playerPosition >= 100)
+                {
+                    Console.WriteLine("Congratulations! You reached the top.");
+                    gameOver = true;
+                }
+            }
+
+            Console.WriteLine("Game Over. Thank you for playing!");
+        }
+    }
+}
